492447441a2aa7e8dd5685c93f7090351d2ac290
Vinu Babu <vinu.b@webandcrafts.com>
2026-04-16 16:18:38 +0530

    fix: use named StrictMode import in main.jsx

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,9 +1,10 @@
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 )
